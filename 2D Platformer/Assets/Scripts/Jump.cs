6af61155427d85481a4f68252168e3e5c2eef6df
Mark Harley <2378551h@student.gla.ac.uk>
2020-10-30 00:25:41 +0000

    Implemented basic jump script

--- 2D Platformer/Assets/Scripts/Jump.cs
+++ 2D Platformer/Assets/Scripts/Jump.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Jump : MonoBehaviour
 {
+    public LayerMask m_ground;
+    public Transform groundCheck;
+    public float jumpForce = 100f;
+    bool jump = false;
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,25 @@ public class Jump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetButtonDown("Jump")){
+            jump = true;
+        }
+    }
+
+
+    void FixedUpdate(){
+        if (CheckforGround() && jump){
+            gameObject.GetComponent<Rigidbody2D>().AddForce(new Vector2(0f,jumpForce));
+            jump = false;
+        }
+    }
+
+    bool CheckforGround(){
+        Collider2D[] hitColliders = Physics2D.OverlapBoxAll(groundCheck.position, gameObject.transform.localScale / 2, 0f,m_ground);
         
+        if (hitColliders.Length > 0){
+            return true;
+        }
+        return false;
     }
 }
